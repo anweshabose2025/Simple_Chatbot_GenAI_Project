69414905a20194a5ddaf5c075cd36443f85e7bbc
Anwesha Bose <anwesha.bbose2025@gmail.com>
2025-11-10 00:26:35 +0530

    Add files via upload

---
+++ 1-app.py
@@ -0,0 +1,39 @@
+# (D:\Udemy\Complete_GenAI_Langchain_Huggingface\Python\venv) 
+# D:\Udemy\Complete_GenAI_Langchain_Huggingface\Python\25-Getting Started with OpenAI and Ollama\3-ollama>streamlit run 1-app.py
+
+from langchain_groq import ChatGroq 
+import streamlit as st 
+from langchain_core.prompts import ChatPromptTemplate 
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+from langchain_core.output_parsers import StrOutputParser
+
+# Groq
+import os
+from dotenv import load_dotenv 
+load_dotenv()
+os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
+
+## streamlit framework
+st.set_page_config("🤖 Chatbot")
+st.header("👾 Simple Chatbot with GPT-OSS Model")
+st.subheader("🖊️ What can I help with?", divider="grey")
+input_text=st.chat_input("Ask anything...")
+
+## Ollama Gemma2b model
+llm=ChatGroq(model="openai/gpt-oss-120b")
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [("system","You are a helpful assistant. Please respond to the question asked"),
+     ("user","Question:{context}")])
+
+## Output parser
+output_parser=StrOutputParser()
+
+## Chain
+chain = prompt | llm | output_parser
+#chain = create_stuff_documents_chain(llm=llm,prompt=prompt,output_parser=output_parser)
+
+if input_text:
+    response = chain.invoke({"context":input_text})
+    st.write(response)
